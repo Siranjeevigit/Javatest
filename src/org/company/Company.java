cec266743f80cab1818a66e7c57a8806c5098aa9
Siranjeevigit <gvsmile10@gmail.com>
2020-01-10 13:00:41 +0530

    new code

--- src/org/company/Company.java
+++ src/org/company/Company.java
@@ -5,6 +5,8 @@ public class Company extends Client
 	public void companyName() 
 	{
 		System.out.println("company name is guru");
+		System.out.println("company name is guru");
+		System.out.println("company name is guru");
 	}
 	
 	public static void main(String[] args) 
